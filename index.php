82f37124f5878c86295f50338bad8af12ca1e293
Alif Ahsan Nibir <ahsan2649@outlook.com>
2022-04-01 14:26:58 +0600

    Episode 3 End

--- index.php
+++ index.php
@@ -1,7 +1,12 @@
 <?php # start php code
+# php index.php -> Run file
+# php -S <addr>:<port> -> serve file at <addr>:<port>
 
+$greeting = 'Hello Universe'; # make a variable
+$name = 'Ahsan';
 
 echo 'Hello World'; # print out a string;
+echo "Hello, $name"; # print out a variable inside a string using double quotes;
+echo "Hello, " . $name; # concatenate strings using .
 
 
-# php index.php -> Run file
