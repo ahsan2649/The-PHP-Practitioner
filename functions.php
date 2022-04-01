4f66c831332d23447f6fd4f233e173e5fb6e9e7c
Alif Ahsan Nibir <ahsan2649@outlook.com>
2022-04-01 20:26:56 +0600

    Episode 10 End

---
+++ functions.php
@@ -0,0 +1,11 @@
+<?php
+
+// define a function
+
+function dd($data){    
+    echo '<pre>';
+
+    die(var_dump($data));
+
+    echo '</pre>';
+}
--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php
 
-require 'index.view.php';
+// keep all functions in a separate file
+require 'functions.php';
+
+$animals  = ['dog', 'cat'];
+
+dd($animals);
+
+// keep all views in a separate file
+require 'index.view.php'; 
