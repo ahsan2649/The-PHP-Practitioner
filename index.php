81c1a468fc71767e0ea44e5316e5c77e8aa7cdf1
Alif Ahsan Nibir <ahsan2649@outlook.com>
2022-04-01 14:52:12 +0600

    Episode 5 End

--- index.php
+++ index.php
@@ -1,37 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Document</title>
-    <style>
-        header{
-            background: #e3e3e3;
-            padding: 2em;
-            text-align:center;
+<?php
 
-        }
-    </style>
-</head>
-<body>
+$greeting = "Hello, World";
 
-    <header>
-        <!--<h1>
-            <?php 
-            
-                // $name = $_GET['name']; # Using super globals
-
-                // echo "Hello, $name"; #/?name=Ahsan
-            
-            ?>
-             close php using ?> 
-        </h1>-->
-
-        <h1>
-            <?="Hello, ". htmlspecialchars($_GET['name']);?>
-            <!--Shorthand for echo, htmlspecialchars is a function that sanitizes input-->
-        </h1>
-
-    </header>
-
-</body>
-</html>
+require 'index.view.php'; // Copy all contents here
---
+++ index.view.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<!-- View for index page -->
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <style>
+        header{
+            background: #e3e3e3;
+            padding: 2em;
+            text-align:center;
+
+        }
+    </style>
+</head>
+<body>
+
+    <header>
+        <!--<h1>
+            <?php 
+            
+                // $name = $_GET['name']; # Using super globals
+
+                // echo "Hello, $name"; #/?name=Ahsan
+            
+            ?>
+             close php using ?> 
+        </h1>-->
+
+        <h1>
+            <?=$greeting;?>
+            <!--Shorthand for echo-->
+        </h1>
+
+    </header>
+
+</body>
+</html>
