0a8c95ce55abc6b5ec8dc13f684f8edd32d537bf
Alif Ahsan Nibir <ahsan2649@outlook.com>
2022-04-01 19:21:07 +0600

    Episode 8 End

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
 
-$task = [];
+$task = [
+    'title' => 'Finish tutorial',
+    'due' => 'this week',
+    'assigned_to' => 'me',
+    'completed' => false // boolean -> true or false
+];
 
 require 'index.view.php';
--- index.view.php
+++ index.view.php
@@ -15,7 +15,15 @@
 </head>
 <body>
 
-        
+        <h1>Task for the day</h1>
+        <ul>
+            <li><strong>Name: </strong> <?= $task['title']; ?></li>
+            <li><strong>Due Date: </strong> <?= $task['due']; ?></li>
+            <li><strong>Person Responsible: </strong> <?= $task['assigned_to']; ?></li>
+            <li><strong>Status: </strong> <?= $task['completed'] ? 'Complete' : 'Incomplete'; ?></li>
+            
+
+        </ul>
 
 </body>
 </html>
