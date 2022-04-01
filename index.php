e0e9380a141292121a0669e101b69a48a5e2dcbc
Alif Ahsan Nibir <ahsan2649@outlook.com>
2022-04-01 14:47:37 +0600

    Episode 4 End

--- index.php
+++ index.php
@@ -3,8 +3,35 @@
 <head>
     <meta charset="UTF-8">
     <title>Document</title>
+    <style>
+        header{
+            background: #e3e3e3;
+            padding: 2em;
+            text-align:center;
+
+        }
+    </style>
 </head>
 <body>
-    
+
+    <header>
+        <!--<h1>
+            <?php 
+            
+                // $name = $_GET['name']; # Using super globals
+
+                // echo "Hello, $name"; #/?name=Ahsan
+            
+            ?>
+             close php using ?> 
+        </h1>-->
+
+        <h1>
+            <?="Hello, ". htmlspecialchars($_GET['name']);?>
+            <!--Shorthand for echo, htmlspecialchars is a function that sanitizes input-->
+        </h1>
+
+    </header>
+
 </body>
 </html>
