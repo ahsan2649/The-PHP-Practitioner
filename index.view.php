84d235d6bca437d7f9cd367c2c7a76cf10fe6aff
Alif Ahsan Nibir <ahsan2649@outlook.com>
2022-04-02 11:52:43 +0600

    Episode 13 Start

--- index.view.php
+++ index.view.php
@@ -5,14 +5,6 @@
     <title>Document</title>
 </head>
 <body>
-    <?php foreach($tasks as $task): ?>
-        <?php if($task-> completed) : ?>
-            <strike><li><?= $task->description;?></li></strike>
-        <?php else: ?>
-        
-            <li><?= $task->description;?></li>
-            
-        <?php endif; ?>
-    <?php endforeach?>
+
 </body>
 </html>
