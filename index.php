6e9d839cc2fbdf9ee0e53eb9454c8ca39ade3776
Alif Ahsan Nibir <ahsan2649@outlook.com>
2022-04-02 11:35:32 +0600

    Episode 12 End

--- index.php
+++ index.php
@@ -2,14 +2,30 @@
 
 // Classes for creating objects
 class Task {
+    public $description;
+    public $completed = false;
     // Constructor runs when object is created
     public function __construct($description){
         $this -> description = $description;
     }
+
+    // Functions are methods in a class
+    public function isComplete(){
+        return $this -> completed;
+    }
+    public function complete(){
+        $this->completed = true;
+    }
 }
 
 // Instantiating an object
-$task = new Task('Go to the store');
+$tasks = [
+    new Task("Go to the store"),
+    new Task('Finish the tutorials'),
+    new Task('Finish learning german')
+];
+
+$tasks[0]->complete();
 
 // keep all views in a separate file
 require 'index.view.php'; 
