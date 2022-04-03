b4941141fafb7919ba36a5786c6a0b6add9046bf
Alif Ahsan Nibir <ahsan2649@outlook.com>
2022-04-03 09:04:56 +0600

    Episode 13 End

--- index.php
+++ index.php
@@ -1,5 +1,15 @@
 <?php
 
+// keep classes in separate files
+require 'Task.php';
+
+// keep functions in a separate file
+require 'functions.php';
+
+$pdo = connectToDb();
+
+$tasks = fetchAllTasks($pdo);
+
 
 
 // keep all views in a separate file
--- index.view.php
+++ index.view.php
@@ -5,6 +5,14 @@
     <title>Document</title>
 </head>
 <body>
-
+    <?php foreach($tasks as $task): ?>
+        <?php if($task-> completed) : ?>
+            <strike><li><?= $task->description;?></li></strike>
+        <?php else: ?>
+        
+            <li><?= $task->description;?></li>
+            
+        <?php endif; ?>
+    <?php endforeach?>
 </body>
 </html>
