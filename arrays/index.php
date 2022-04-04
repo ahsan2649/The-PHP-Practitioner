0c515ad402bba9f8786aca81cf21ff1cf81d0bc1
Alif Ahsan Nibir <ahsan2649@outlook.com>
2022-04-04 14:59:11 +0600

    Episode 18 End

--- arrays/index.php
+++ arrays/index.php
@@ -4,21 +4,37 @@ class Post
 {
     public $title;
     public $published;
+    public $author;
 
-
-    public function __construct($title, $published)
+    public function __construct($title, $author, $published)
     {
         $this->title = $title;
+        $this->author = $author;
         $this->published = $published;
     }
 }
 
 $posts = [
-    new Post("My First Post", true),
-    new Post("My Seconde Post", true),
-    new Post("My Third Post", true),
-    new Post("My Fourth Post", false),
+    new Post("My First Post", 'JW',true),
+    new Post("My Seconde Post", 'AAN',true),
+    new Post("My Third Post", 'SI',true),
+    new Post("My Fourth Post", 'FR',false),
 
 ];
 
-var_dump($posts);
+// array_filter: filter posts depending on condition
+// $unpublishedPosts = array_filter($posts, function($post){
+//     return !$post->published;
+// });
+
+// $publishedPosts = array_filter($posts, function($post){
+//     return $post->published;
+// });
+
+// array_map: modified array
+// $modified = array_map(function($post){
+//     return ['title' => $post->title];
+// }, $posts);
+
+// array_column: array of values under a key
+// $titles = array_column($posts, 'author', 'writer');
