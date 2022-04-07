073681848db7668e1cfb96bc141d69af306f7015
ahsan2649 <ahsan2649@outlook.com>
2022-04-07 22:30:15 +0600

    Adding comments

--- index.php
+++ index.php
@@ -3,5 +3,8 @@
 // put all bootstrapping in one file
 require 'core/bootstrap.php';
 
+
+
+// Loading routes into router, and directing
 require Router::load('routes.php')
     ->direct(Request::uri());
